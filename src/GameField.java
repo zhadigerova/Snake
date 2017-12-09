ad6cce0760583fc56a4d9ebf08a917d9ae0eb17c
mira <m.zhadigerova@gmail.com>
2017-12-10 01:21:04 +0300

    Snake can move and eat the apple.

--- src/GameField.java
+++ src/GameField.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
 
-public class GameField extends JPanel {
+public class GameField extends JPanel implements ActionListener{
     private final int SIZE = 320;
     private final int DOT_SIZE = 16;
     private final int ALL_DOTS = 400;
@@ -22,6 +27,25 @@ public class GameField extends JPanel {
     public GameField(){
         setBackground(Color.BLACK);
         loadImages();
+        initGame();
+        addKeyListener(new FieldKeyListener());
+        setFocusable(true);
+    }
+
+    public void initGame(){
+        dots = 3;
+        for (int i = 0; i < dots; i++) {
+            x[i] = 48 - i*DOT_SIZE;
+            y[i] = 48;
+        }
+        timer = new Timer(250, this);
+        timer.start();
+        createApple();
+    }
+
+    public void createApple(){
+        appleX = new Random().nextInt(20) * DOT_SIZE;
+        appleY = new Random().nextInt(20) * DOT_SIZE;
     }
 
     public void loadImages(){
@@ -29,5 +53,114 @@ public class GameField extends JPanel {
         apple = iia.getImage();
         ImageIcon iid = new ImageIcon("dot.png");
         dot = iid.getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        if (inGame){
+            graphics.drawImage(apple, appleX, appleY, this);
+            for (int i = 0; i < dots; i++) {
+                graphics.drawImage(dot, x[i], y[i], this);
+            }
+        }
+        else {
+            String str = "Game Over";
+            //Font f = new Font("Arial", 14, Font.BOLD);
+            graphics.setColor(Color.WHITE);
+            //graphics.setFont(f);
+            graphics.drawString(str, 125, SIZE/2);
+        }
+    }
+
+    public void move(){
+        for (int i = dots; i > 0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        if (left){
+            x[0] -= DOT_SIZE;
+        }
+        if (right){
+            x[0] += DOT_SIZE;
+        }
+        if (up){
+            y[0] -= DOT_SIZE;
+        }
+        if (down){
+            y[0] += DOT_SIZE;
+        }
+    }
+    public void checkApple(){
+        //если координаты головы змейки равны координатам яблока
+        if (x[0] == appleX && y[0] == appleY){
+            dots++;
+            //тогда создаем новое яблоко
+            createApple();
+        }
+    }
+
+
+    public void checkCollisions(){
+        // змейка столкнулась сама с собой
+        for (int i = dots; i > 0 ; i--) {
+            // >4 - только если длина змейки больше 4 возможно столкновение
+            if (i>4 && x[0] == x[i] && y[0] == y[i]){
+                inGame = false;
+            }
+        }
+
+        // если змейка больше игрового поля
+        if (x[0] > SIZE){
+            inGame = false;
+        }
+        if (x[0] < 0){
+            inGame = false;
+        }
+        if (y[0] > SIZE){
+            inGame = false;
+        }
+        if (y[0] < 0){
+            inGame = false;
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (inGame){
+            checkApple();
+            checkCollisions();
+            move();
+        }
+        repaint();
+    }
+
+    class FieldKeyListener extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent keyEvent) {
+            super.keyPressed(keyEvent);
+            int key = keyEvent.getKeyCode();
+            if (key == KeyEvent.VK_LEFT && !right){
+                left = true;
+                up = false;
+                down = false;
+            }
+            if (key == KeyEvent.VK_RIGHT && !left){
+                right = true;
+                up = false;
+                down = false;
+            }
+            if (key == KeyEvent.VK_UP && !down){
+                right = false;
+                up = true;
+                down = false;
+            }
+            if (key == KeyEvent.VK_DOWN && !up){
+                right = false;
+                up = false;
+                down = true;
+            }
+
+        }
     }
 }
